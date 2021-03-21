7b03267fbfa06b4c655e08264dac97f2c6ea1a58
Simon <scdg98@outlook.com>
2021-03-21 01:29:05 -0400

    Done the lab 9

--- Assets/Data/SceneDataSO.cs
+++ Assets/Data/SceneDataSO.cs
@@ -7,6 +7,7 @@ public class SceneDataSO : ScriptableObject
 {
     //player Data
     public Vector3 playerPosition;
+    public Quaternion playerRotation;
     public int playerHealth;
         
 }
